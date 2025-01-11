8881afa365baecb0c5b4827e175257d6b05bf320
Laddge <dev.laddge@gmail.com>
2025-01-12 00:19:16 +0900

    feat: use atan2

--- src/components/Arrow.svelte
+++ src/components/Arrow.svelte
@@ -11,7 +11,7 @@
     [(axis[1][0] + axis[1][2]) / 2, (axis[1][1] + axis[1][3]) / 2],
   ])
   let length = $derived(((points[1][0] - points[0][0]) ** 2 + (points[1][1] - points[0][1]) ** 2) ** .5 - 1.5)
-  let rotate = $derived(-Math.atan((points[1][1] - points[0][1]) / (points[0][0] - points[1][0])) + (points[0][0] - points[1][0] < 0 ? 0 : Math.PI))
+  let rotate = $derived(Math.atan2(points[1][1] - points[0][1], points[1][0] - points[0][0]))
 </script>
 
 <div class="absolute flex justify-center items-center" style={`width: ${length / config.size[0] * 100}%; height: ${2 / config.size[1] * 50}%; left: ${(points[0][0] + points[1][0]) / 2 / config.size[0] * 100}%; top: ${(points[0][1] + points[1][1]) / 2 / config.size[1] * 100}%; transform: translate(-50%, -50%) rotate(${rotate}rad);`}>
